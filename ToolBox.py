346d007aebf1e7b4267643b40aaf718a230f41f8
Zoe Abernathy <zoeabernathy@outlook.com>
2021-07-30 00:11:51 +0200

    Updated toolbox to version 2.0

--- ToolBox.py
+++ ToolBox.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "Tool Collection (ToolBox)",
     "author": "ZoZo Zoe",
-    "version": (1, 0),
+    "version": (2, 0),
     "blender": (2, 80, 0),
     "location": "View 3D > Tool Shelf > Tool Collection",
     "description": "A collection of simple but handy tools",
@@ -24,9 +24,15 @@ from bpy.props import(StringProperty)
 #               ToolBox Properties
 #----------------------------------------------
 
-class ToolBoxPropertyGroup(bpy.types.PropertyGroup):
+class ToolBoxMeshGroup(bpy.types.PropertyGroup):
     Armature: bpy.props.PointerProperty(type=bpy.types.Object)
     Mesh: bpy.props.PointerProperty(type=bpy.types.Object)
+    
+class ToolBoxWeightpaintGroup(bpy.types.PropertyGroup):
+    Armature: bpy.props.PointerProperty(type=bpy.types.Object)
+    Mesh: bpy.props.PointerProperty(type=bpy.types.Object)
+    SecondMesh: bpy.props.PointerProperty(type=bpy.types.Object)
+    Bone: bpy.props.StringProperty()
 
 
 
@@ -88,9 +94,6 @@ class ShapekeyTools(bpy.types.Panel):
 class OBJECT_OT_shapekeyOp(Operator):
     bl_label = "Select shapekey verts"
     bl_idname = "object.shapekeyop"
-    bl_space_type = 'VIEW_3D'
-    bl_region_type = 'UI'
-    bl_category = "Tool Collection"
     bl_option = {'REGISTER', 'UNDO'}
     
 
@@ -127,9 +130,6 @@ class OBJECT_OT_shapekeyOp(Operator):
 class OBJECT_OT_returnshapeOp(Operator):
     bl_label = "Return verts to shapekey"
     bl_idname = "object.returnshapeop"
-    bl_space_type = 'VIEW_3D'
-    bl_region_type = 'UI'
-    bl_category = "Tool Collection"
     bl_option = {'REGISTER', 'UNDO'}
     
     @classmethod
@@ -184,15 +184,31 @@ class WeightpaintingTools(bpy.types.Panel):
         col = box.column(align=True)
         split = col.row(align=True)
         row = split.row(align=True)
-        row.scale_y = 1.5
-        row.label(text="Transfer weights mesh to mesh")
-        row.operator("object.transweight", icon='MOD_VERTEX_WEIGHT')
+        row.scale_y = 1
+        row.label(text="Update object:")
+        row.operator("object.isselweightbone", text="", icon='SELECT_SET')
+        row.operator("object.isselweightmesh", text="", icon='SELECT_SET')
+        row.operator("object.isselweightsecmesh", text="", icon='SELECT_SET')
+        
+        split = col.row(align=True)
+        row = split.row(align=True)
+        row.scale_y = 1
+        row.label(text="")
+        row.operator("object.selweightbone", text="", icon='BONE_DATA')
+        row.operator("object.selweightmesh", text="", icon='SHAPEKEY_DATA')
+        row.operator("object.selweightsecmesh", text="", icon='SHAPEKEY_DATA')
         
         split = col.row(align=True)
         row = split.row(align=True)
         row.scale_y = 1.5
-        row.label(text="Transfer weights from bone")
-        row.operator("object.transweightsingle", icon='BONE_DATA')
+        row.label(text="Transfer weights from:")
+        split = col.row(align=True)
+        row = split.row(align=True)
+        row.scale_y = 1.5
+        row.operator("object.transweight",text="Mesh", icon='MOD_VERTEX_WEIGHT')
+        row.operator("object.transweightsingle",text="Bone", icon='BONE_DATA')
+        
+
 
 #----------------------------------------------
 #               Transfer Weights
@@ -201,17 +217,20 @@ class WeightpaintingTools(bpy.types.Panel):
 class OBJECT_OT_transweights(Operator):
     bl_label = "Transfer Weights"
     bl_idname = "object.transweight"
-    bl_space_type = 'VIEW_3D'
-    bl_region_type = 'UI'
-    bl_category = "Tool Collection"
     bl_option = {'REGISTER', 'UNDO'}
     
     @classmethod
     def poll(cls, context):
-        return context.object.select_get() and context.object.type == 'MESH'
+        return bpy.context.scene.ToolBoxWeight.Mesh and bpy.context.scene.ToolBoxWeight.SecondMesh
  
         
     def execute(self, context):
+        
+        bpy.ops.object.mode_set(mode='OBJECT', toggle=False)
+        bpy.ops.object.select_all(action='DESELECT')
+        bpy.context.scene.ToolBoxWeight.Mesh.select_set(True)
+        bpy.context.scene.ToolBoxWeight.SecondMesh.select = True
+        
         bpy.ops.object.mode_set(mode='WEIGHT_PAINT', toggle=False)
         bpy.ops.object.data_transfer(use_reverse_transfer=True, data_type='VGROUP_WEIGHTS', layers_select_src='NAME', layers_select_dst='ALL')
         bpy.ops.object.mode_set(mode='OBJECT', toggle=False)
@@ -225,18 +244,36 @@ class OBJECT_OT_transweights(Operator):
 class OBJECT_OT_transweightsSingle(Operator):
     bl_label = "Transfer Weights Single"
     bl_idname = "object.transweightsingle"
-    bl_space_type = 'VIEW_3D'
-    bl_region_type = 'UI'
-    bl_category = "Tool Collection"
     bl_option = {'REGISTER', 'UNDO'}
     
     @classmethod
     def poll(cls, context):
-        return context.object.select_get() and context.object.type == 'MESH'
+        return bpy.context.scene.ToolBoxWeight.Mesh and bpy.context.scene.ToolBoxWeight.SecondMesh
  
     def execute(self, context):
+        bpy.ops.object.mode_set(mode='OBJECT', toggle=False)
+        bpy.ops.object.select_all(action='DESELECT')
+        bpy.context.view_layer.objects.active = bpy.context.scene.ToolBoxWeight.Armature
+        bpy.context.scene.ToolBoxWeight.Armature.select = True
+                
+        bpy.ops.object.mode_set(mode='POSE', toggle=False)
+                
+        for i in bpy.context.selected_pose_bones:
+            bpy.context.object.data.bones[i.name].select = False
 
-        bpy.ops.object.data_transfer(use_reverse_transfer=True, data_type='VGROUP_WEIGHTS',  vert_mapping='NEAREST', layers_select_src='NAME', layers_select_dst='BONE_SELECT', mix_mode='REPLACE')
+        bpy.context.object.data.bones[bpy.context.scene.ToolBoxWeight.Bone].select = True
+
+        bpy.ops.object.mode_set(mode='OBJECT', toggle=False)
+
+        bpy.context.scene.ToolBoxWeight.Mesh.select_set(True)
+        bpy.context.scene.ToolBoxWeight.SecondMesh.select = True
+        bpy.context.view_layer.objects.active = bpy.context.scene.ToolBoxWeight.SecondMesh
+        bpy.ops.object.mode_set(mode='WEIGHT_PAINT', toggle=False)
+
+        bpy.ops.object.data_transfer(use_reverse_transfer=True, data_type='VGROUP_WEIGHTS',  layers_select_src='NAME', layers_select_dst='BONE_SELECT', mix_mode='REPLACE')
+        
+        bpy.ops.object.mode_set(mode='OBJECT', toggle=False)
+        bpy.ops.object.select_all(action='DESELECT')
 
 
         return {"FINISHED"}
@@ -284,59 +321,22 @@ class MeshMenu(bpy.types.Panel):
 
         split = col.row(align=True)
         row = split.row(align=True)
-        row.scale_y = 1.5
-        row.label(text="Update object:")
-        row.operator("object.selarmature", icon='MESH_DATA')
-        row.operator("object.selmesh", icon='SHAPEKEY_DATA')
+        row.scale_y = 1
+        row.label(text="Selector:")
+        row.operator("object.isselmesharmature", text="", icon='SELECT_SET')
+        row.operator("object.isselmeshmesh", text="", icon='SELECT_SET')
+
+        split = col.row(align=True)
+        row = split.row(align=True)
+        row.scale_y = 1
+        row.label(text="")
+        row.operator("object.selmesharmature", text="", icon='MESH_DATA')
+        row.operator("object.selmeshmesh", text="", icon='SHAPEKEY_DATA')
 
         split = col.row(align=True)
         row = split.row(align=True)
         row.scale_y = 1.5
         row.operator("object.remvertgroups", icon='OUTLINER_DATA_MESH')
-
-#----------------------------------------------
-#               Update Material
-#----------------------------------------------
-    
-class OBJECT_OT_SelectArmatureOp(Operator):
-    bl_label = "Armature"
-    bl_idname = "object.selarmature"
-    bl_space_type = 'VIEW_3D'
-    bl_region_type = 'UI'
-    bl_category = "Tool Collection Beta"
-    bl_option = {'REGISTER', 'UNDO'}
-
-    @classmethod
-    def poll(cls, context):
-        return context.object.select_get() and context.object.type == 'ARMATURE'
-
-    def execute(self, context):
-        
-        ob = bpy.context.object
-        bpy.context.scene.ToolBox.Armature = ob
-        return {"FINISHED"}
-
-#----------------------------------------------
-#               Update Vertex
-#----------------------------------------------
-    
-class OBJECT_OT_SelectMeshOp(Operator):
-    bl_label = "Mesh"
-    bl_idname = "object.selmesh"
-    bl_space_type = 'VIEW_3D'
-    bl_region_type = 'UI'
-    bl_category = "Tool Collection Beta"
-    bl_option = {'REGISTER', 'UNDO'}
-
-    @classmethod
-    def poll(cls, context):
-        return context.object.select_get() and context.object.type == 'MESH'
-    
-    def execute(self, context):
-        
-        ob = bpy.context.object
-        bpy.context.scene.ToolBox.Mesh = ob
-        return {"FINISHED"}
     
     
 #----------------------------------------------
@@ -346,18 +346,15 @@ class OBJECT_OT_SelectMeshOp(Operator):
 class OBJECT_OT_RemoveVertexGroupsOp(Operator):
     bl_label = "Remove Vertex Groups"
     bl_idname = "object.remvertgroups"
-    bl_space_type = 'VIEW_3D'
-    bl_region_type = 'UI'
-    bl_category = "Tool Collection Beta"
     bl_option = {'REGISTER', 'UNDO'}
 
     @classmethod
     def poll(cls, context):
-        return bpy.context.scene.ToolBox.Armature and bpy.context.scene.ToolBox.Mesh
+        return bpy.context.scene.ToolBoxMesh.Armature and bpy.context.scene.ToolBoxMesh.Mesh
     
     def execute(self, context):
         Arma = bpy.context.scene.ToolBox.Armature
-        Mesh = bpy.context.scene.ToolBox.Mesh
+        Mesh = bpy.context.scene.ToolBoxMesh
         VertexKeys = Mesh.vertex_groups.keys()
         BoneKeys = Arma.data.bones.keys()
         
@@ -370,18 +367,238 @@ class OBJECT_OT_RemoveVertexGroupsOp(Operator):
                 Mesh.vertex_groups.remove(RemoveVertexGroup)
                 print("removed " + key)
                 
-
-        bpy.context.scene.ToolBox.property_unset('Armature')
-        bpy.context.scene.ToolBox.property_unset('Mesh')
         
         return {"FINISHED"}
+
+
+
+
+#---------------------------------------------- Selection updates ----------------------------------------------
+
+#---------------------------------------------- Mesh ----------------------------------------------     
+#----------------------------------------------
+#               Update Armature
+#----------------------------------------------
+    
+class OBJECT_OT_SelectMeshArmatureOp(Operator):
+    bl_label = "Select Armature"
+    bl_idname = "object.selmesharmature"
+    bl_option = {'REGISTER', 'UNDO'}
+
+    @classmethod
+    def poll(cls, context):
+        return context.object.select_get() and context.object.type == 'ARMATURE'
+
+    def execute(self, context):
+        
+        ob = bpy.context.object
+        bpy.context.scene.ToolBoxMesh.Armature = ob
+        return {"FINISHED"}
+    
+
+class OBJECT_OT_ISSelectMeshArmatureOp(Operator):
+    bl_label = "Select Armature"
+    bl_idname = "object.isselmesharmature"
+    bl_option = {'REGISTER', 'UNDO'}
+
+    @classmethod
+    def poll(cls, context):
+        return bpy.context.scene.ToolBoxMesh.Armature
+
+    def execute(self, context):
+        
+        bpy.ops.object.mode_set(mode='OBJECT', toggle=False)
+        bpy.ops.object.select_all(action='DESELECT')
+        bpy.context.scene.ToolBoxMesh.Armature.select_set(True)
+        
+        return {"FINISHED"}
+       
+
+#----------------------------------------------
+#               Update Mesh
+#----------------------------------------------
+    
+class OBJECT_OT_SelectMeshOp(Operator):
+    bl_label = "Select Mesh"
+    bl_idname = "object.selmeshmesh"
+    bl_option = {'REGISTER', 'UNDO'}
+
+    @classmethod
+    def poll(cls, context):
+        return context.object.select_get() and context.object.type == 'MESH'
+    
+    def execute(self, context):
+        
+        ob = bpy.context.object
+        bpy.context.scene.ToolBoxMesh.Mesh = ob
+        return {"FINISHED"}
+ 
+    
+class OBJECT_OT_ISSelectMeshOp(Operator):
+    bl_label = "Select Mesh"
+    bl_idname = "object.isselmeshmesh"
+    bl_option = {'REGISTER', 'UNDO'}
+    
+    @classmethod
+    def poll(cls, context):
+        return bpy.context.scene.ToolBoxMesh.Mesh
+
+    def execute(self, context):
+        
+        bpy.ops.object.mode_set(mode='OBJECT', toggle=False)
+        bpy.ops.object.select_all(action='DESELECT')
+        bpy.context.scene.ToolBoxMesh.Mesh.select_set(True)
+        
+        return {"FINISHED"}
+
+
+#---------------------------------------------- Weights ----------------------------------------------  
+#----------------------------------------------
+#               Update Mesh
+#----------------------------------------------
+    
+class OBJECT_OT_SelectWeightMeshOp(Operator):
+    bl_label = "Source Mesh"
+    bl_idname = "object.selweightmesh"
+    bl_option = {'REGISTER', 'UNDO'}
+
+    @classmethod
+    def poll(cls, context):
+        return context.object.select_get() and context.object.type == 'MESH'
+    
+    def execute(self, context):
+        
+        ob = bpy.context.object
+        bpy.context.scene.ToolBoxWeight.Mesh = ob
+        return {"FINISHED"}
+ 
+    
+class OBJECT_OT_ISMeshWeightSelectMeshOp(Operator):
+    bl_label = "Select Source Mesh"
+    bl_idname = "object.isselweightmesh"
+    bl_option = {'REGISTER', 'UNDO'}
+    
+    @classmethod
+    def poll(cls, context):
+        return bpy.context.scene.ToolBoxWeight.Mesh
+
+    def execute(self, context):
+        
+        bpy.ops.object.mode_set(mode='OBJECT', toggle=False)
+        bpy.ops.object.select_all(action='DESELECT')
+        bpy.context.scene.ToolBoxWeight.Mesh.select_set(True)
+        
+        return {"FINISHED"}
+    
+#----------------------------------------------
+#               Secondary Mesh
+#----------------------------------------------
+    
+class OBJECT_OT_SelectWeightSecondMeshOp(Operator):
+    bl_label = "Destination Mesh"
+    bl_idname = "object.selweightsecmesh"
+    bl_option = {'REGISTER', 'UNDO'}
+
+    @classmethod
+    def poll(cls, context):
+        return context.object.select_get() and context.object.type == 'MESH'
+    
+    def execute(self, context):
+        
+        ob = bpy.context.object
+        bpy.context.scene.ToolBoxWeight.SecondMesh = ob
+        return {"FINISHED"}
+    
+
+class OBJECT_OT_ISSelectWeightSecondMeshOp(Operator):
+    bl_label = "Select Destination Mesh"
+    bl_idname = "object.isselweightsecmesh"
+    bl_option = {'REGISTER', 'UNDO'}
+
+    @classmethod
+    def poll(cls, context):
+        return bpy.context.scene.ToolBoxWeight.SecondMesh
+
+    def execute(self, context):
+        
+        bpy.ops.object.mode_set(mode='OBJECT', toggle=False)
+        bpy.ops.object.select_all(action='DESELECT')
+        bpy.context.scene.ToolBoxWeight.SecondMesh.select_set(True)
+        
+        return {"FINISHED"}
+    
+    
+#----------------------------------------------
+#               Select Bone
+#----------------------------------------------
+    
+class OBJECT_OT_SelectWeightBoneOp(Operator):
+    bl_label = "Select Bone"
+    bl_idname = "object.selweightbone"
+    bl_option = {'REGISTER', 'UNDO'}
+
+    @classmethod
+    def poll(cls, context):
+        return context.object.select_get() and context.object.type == 'ARMATURE' and bpy.context.object.mode == 'EDIT' or bpy.context.object.mode == 'POSE'
+    
+    def execute(self, context):
+        
+        if bpy.context.object.mode == 'EDIT':
+            if len(bpy.context.selected_bones) !=0:
+                bpy.context.scene.ToolBoxWeight.Armature = bpy.context.object
+                bpy.context.scene.ToolBoxWeight.Bone = bpy.context.selected_bones[0].name
+            else:
+                self.report({"WARNING"}, "No bone was selected, please select one")
+                
+            if len(bpy.context.selected_bones) > 1:
+                self.report({"INFO"}, "Multiple bones selected. The first bone was selected: " + bpy.context.scene.ToolBoxWeight.Bone)
+            
+        if bpy.context.object.mode == 'POSE':
+            if len(bpy.context.selected_pose_bones) != 0:
+                bpy.context.scene.ToolBoxWeight.Armature = bpy.context.object
+                bpy.context.scene.ToolBoxWeight.Bone = bpy.context.selected_pose_bones[0].name
+            else:    
+                self.report({"WARNING"}, "No bone was selected, please select one")
+                
+            if len(bpy.context.selected_pose_bones) > 1:
+                self.report({"INFO"}, "Multiple bones selected. The first bone was selected: " + bpy.context.scene.ToolBoxWeight.Bone)
+            
+        return {"FINISHED"}
+    
+
+class OBJECT_OT_ISSelectWeightBoneOp(Operator):
+    bl_label = "Select Bone"
+    bl_idname = "object.isselweightbone"
+    bl_option = {'REGISTER', 'UNDO'}
+
+    @classmethod
+    def poll(cls, context):
+        return bpy.context.scene.ToolBoxWeight.Bone
+
+    def execute(self, context):
+        
+        bpy.ops.object.mode_set(mode='OBJECT', toggle=False)
+        bpy.ops.object.select_all(action='DESELECT')
+        bpy.context.view_layer.objects.active = bpy.context.scene.ToolBoxWeight.Armature
+        bpy.context.scene.ToolBoxWeight.Armature.select = True
+        
+        bpy.ops.object.mode_set(mode='POSE', toggle=False)
+        
+        for i in bpy.context.selected_pose_bones:
+            bpy.context.object.data.bones[i.name].select = False
+
+        bpy.context.object.data.bones[bpy.context.scene.ToolBoxWeight.Bone].select = True
+        
+        return {"FINISHED"}  
+    
 
 #----------------------------------------------
 #               Registering panel
 #----------------------------------------------
 
 classes = (
-    ToolBoxPropertyGroup,
+    ToolBoxMeshGroup,
+    ToolBoxWeightpaintGroup,
     ToolboxMainMenu,
     MeshMenu,
     ShapekeyTools,
@@ -392,7 +609,15 @@ classes = (
     OBJECT_OT_transweightsSingle,
     OBJECT_OT_RemoveVertexGroupsOp,
     OBJECT_OT_SelectMeshOp,
-    OBJECT_OT_SelectArmatureOp,
+    OBJECT_OT_ISSelectMeshOp,
+    OBJECT_OT_SelectMeshArmatureOp,
+    OBJECT_OT_ISSelectMeshArmatureOp,
+    OBJECT_OT_SelectWeightMeshOp,
+    OBJECT_OT_ISMeshWeightSelectMeshOp,
+    OBJECT_OT_SelectWeightSecondMeshOp,
+    OBJECT_OT_ISSelectWeightSecondMeshOp,
+    OBJECT_OT_SelectWeightBoneOp,
+    OBJECT_OT_ISSelectWeightBoneOp,
 )
     
 
@@ -401,7 +626,8 @@ def register():
     for cls in classes:
         bpy.utils.register_class(cls)
         
-    bpy.types.Scene.ToolBox = bpy.props.PointerProperty(type=ToolBoxPropertyGroup)
+    bpy.types.Scene.ToolBoxMesh = bpy.props.PointerProperty(type=ToolBoxMeshGroup)
+    bpy.types.Scene.ToolBoxWeight = bpy.props.PointerProperty(type=ToolBoxWeightpaintGroup)
         
 def unregister():
     for cls in classes:
